e3e726a359cfc2be48a1d206ba4d4b68874cd82d
Grant Caesar Peters <gcpeters@gmail.com>
2025-01-14 16:43:22 -0500

    removed sanity test

--- specs/remove-adjacent-strings.spec.js
+++ specs/remove-adjacent-strings.spec.js
@@ -12,7 +12,6 @@ const removeAdjacentDupes = (value) => {
 }
 
 test('Remove Adjacement Duplicates in String', () => {
-    expect('hello'.slice(0, 2)).toBe('he');
     expect(removeAdjacentDupes('acddca')).toBe('');
     expect(removeAdjacentDupes('acddce')).toBe('ae');
 });
